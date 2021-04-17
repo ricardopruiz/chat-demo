303f47f52a47820dcac8eb0278dbf8554204e5d4
Ricardo Ruiz <ricardopedroruiz@gmail.com>
2021-04-17 17:10:01 +0200

    BaseInput component developed

--- src/components/baseComponents/BaseChatInput.vue
+++ src/components/baseComponents/BaseChatInput.vue
@@ -1,18 +1,42 @@
 <template>
-  <div class="input-message">
-    <input class="input-element" type="text" />
-  </div>
+  <input
+    type="text"
+    class="input-element"
+    :placeholder="placeHolder"
+    @input="handleInput"
+    :value="value"
+  />
 </template>
 
 <script>
-export default {};
+export default {
+  props: {
+    value: {
+      type: String,
+      default: "",
+    },
+  },
+
+  data() {
+    return {
+      placeHolder: "Escribe un mensaje"
+    };
+  },
+
+  methods: {
+    handleInput(e) {
+      this.$emit("input", e.target.value);
+    },
+  },
+};
 </script>
 
 <style lang="scss">
-.input-message {
-  width: 100;
-  .input-element {
-    border: 0px;
-  }
+.input-element {
+  border:0;
+  outline: none;
+  padding: 30px;
+  width: 100%;
+  height: 100%;
 }
 </style>
