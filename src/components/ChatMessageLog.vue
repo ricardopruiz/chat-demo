0cf628ab60b5404730c5b28257a27fa85b63f3a8
Ricardo Ruiz <ricardopedroruiz@gmail.com>
2021-04-18 13:53:40 +0200

    added message log

---
+++ src/components/ChatMessageLog.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="message-log">
+    <BaseChatMessage
+      v-for="message in conversationMessages"
+      :key="message.id"
+      :messageUser="message.user"
+      :messageText="message.text"
+      :messageReceived="message.isReceived"
+    />
+  </div>
+</template>
+
+<script>
+import BaseChatMessage from "./baseComponents/BaseChatMessage";
+
+export default {
+  components: {
+    BaseChatMessage,
+  },
+
+  props: {
+    conversationMessages: {
+      type: Array,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+.message-log {
+  overflow: hidden;
+  padding: 30px;
+}
+</style>
