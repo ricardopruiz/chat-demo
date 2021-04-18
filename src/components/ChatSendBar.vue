d6af468622ea9475abd030c8f4e0964007bb7f58
Ricardo Ruiz <ricardopedroruiz@gmail.com>
2021-04-18 11:03:54 +0200

    added new sendBar component

---
+++ src/components/ChatSendBar.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="send-bar">
+    <BaseChatInput v-model="text" />
+    <BaseChatButton class="send-button" fontAwesomeIcon="paper-plane" @click="getClicked"/>
+  </div>
+</template>
+
+<script>
+import BaseChatInput from "./baseComponents/BaseChatInput";
+import BaseChatButton from "./baseComponents/BaseChatButton";
+export default {
+  components: {
+    BaseChatInput,
+    BaseChatButton,
+  },
+
+  data(){
+    return {
+      text: undefined
+    }
+  },
+  methods: {
+    getClicked() {
+      console.log("click event");
+    }
+  }
+};
+</script>
+
+<style lang="scss">
+.send-bar {
+  display: flex;
+  flex-wrap: nowrap;
+  
+  border: 1px solid lightgray;
+}
+</style>
